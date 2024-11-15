6cfe496d06ec80a55d6ee98b63f53d52f3b645f0
Jack Hughes <jack.hughes@techteapot.com>
2024-11-15 18:54:16 +0000

    Add robots.txt file to site

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = async function(eleventyConfig) {
   // Copy the contents of the public directory to output directory stripping off the public part
-  eleventyConfig.addPassthroughCopy({"./public/": "/"});
+  eleventyConfig.addPassthroughCopy({ "./public/": "/" });
+
+  // Put robots.txt in root
+  eleventyConfig.addPassthroughCopy({ 'robots.txt': '/robots.txt' });
 };
