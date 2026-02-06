e558ddd7a7f3391e4da7bbbf159e106f37ad5015
digitalbricklayer <jackhughes409@gmail.com>
2026-02-06 17:54:36 +0000

    Contact form put the email back

--- worker/index.js
+++ worker/index.js
@@ -7,17 +7,16 @@ export default {
         const recipient = "jackhughes409@gmail.com";
 
         const formData = await request.formData();
-//        console.log("Form Data:", Object.fromEntries(formData.entries()));
         const name = formData.get("name");
         const email = formData.get("email");
         const message = formData.get("message");
 
-//        await env.SEND_EMAIL.send({
-//            to: [{ email: `${recipient}` }],
-//            from: { email: `${sender}`, name: `${senderName}` },
-//            subject: `Message from ${name}`,
-//            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-//        });
+        await env.SEND_EMAIL.send({
+            to: [{ email: `${recipient}` }],
+            from: { email: `${sender}`, name: `${senderName}` },
+            subject: `Message from ${name}`,
+            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+        });
 
         return Response.redirect("https://leedsnightowls.com/thankyou/", 302);
     }
