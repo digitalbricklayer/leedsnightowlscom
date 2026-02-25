6186eec89d024582e825ab47cd1261f3abce9933
digitalbricklayer <jackhughes409@gmail.com>
2026-02-25 13:04:24 +0000

    Fix formatting in 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = async function(eleventyConfig) {
-  // Copy the contents of the public directory to output directory stripping off the public part
-  eleventyConfig.addPassthroughCopy({ "./public/": "/" });
-  eleventyConfig.addPassthroughCopy({ 'robots.txt': '/robots.txt' });
-  eleventyConfig.addPassthroughCopy({ 'favicon.ico': '/favicon.ico' });
+	// Copy the contents of the public directory to output directory stripping off the public part
+	eleventyConfig.addPassthroughCopy({ "./public/": "/" });
+	eleventyConfig.addPassthroughCopy({ 'robots.txt': '/robots.txt' });
+	eleventyConfig.addPassthroughCopy({ 'favicon.ico': '/favicon.ico' });
 
-  eleventyConfig.addWatchTarget("css/**/*.css");
+	eleventyConfig.addWatchTarget("css/**/*.css");
 
 	// Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
 	// Bundle <style> content and adds a {% css %} paired shortcode
