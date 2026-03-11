2864eea58effbf64df53d33fd29c9f8334872ee2
digitalbricklayer <jackhughes409@gmail.com>
2026-03-11 12:13:08 +0000

    Remove commented out pass through copy for all images

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,6 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import pluginGallery from "./modules/eleventy-plugin-gallery/.eleventy.js";
 
 export default (eleventyConfig) => {
-	// Copy the contents of the public directory to output directory stripping off the public part
-//	eleventyConfig.addPassthroughCopy({ "./public/": "/" });
 	eleventyConfig.addPassthroughCopy({ 'robots.txt': '/robots.txt' });
 	eleventyConfig.addPassthroughCopy({ 'favicon.ico': '/favicon.ico' });
 
