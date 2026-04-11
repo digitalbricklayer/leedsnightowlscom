11763eb769059311e480f0f9a577b9cbead95928
digitalbricklayer <jackhughes409@gmail.com>
2026-04-11 15:18:42 +0100

    Move site content into src dir

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,11 +7,11 @@ import pluginShortcodes from "./_config/shortcodes.js";
 export default (eleventyConfig) => {
 	eleventyConfig.setLayoutsDirectory("_layouts");
 
-	eleventyConfig.addPassthroughCopy({ 'robots.txt': '/robots.txt' });
-	eleventyConfig.addPassthroughCopy({ 'favicon.ico': '/favicon.ico' });
-	eleventyConfig.addPassthroughCopy({ '_redirects': '/_redirects' });
+	eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
+	eleventyConfig.addPassthroughCopy({ 'src/favicon.ico': '/favicon.ico' });
+	eleventyConfig.addPassthroughCopy({ 'src/_redirects': '/_redirects' });
 
-	eleventyConfig.addWatchTarget("css/**/*.css");
+	eleventyConfig.addWatchTarget("src/_includes/css/**/*.css");
 
 	// Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
 	// Bundle <style> content and adds a {% css %} paired shortcode
@@ -29,35 +29,41 @@ export default (eleventyConfig) => {
 	eleventyConfig.addPlugin(pluginShortcodes);
 
 	/**
-     * Add collections - NOTE: do not put dashes in the collection name, otherwise 
+     * Add collections - NOTE: do not put dashes in the collection name, otherwise
 	 * it will not work in Nunjucks templates
      */
-  
+
 	// Return all campaigns
 	eleventyConfig.addCollection("campaigns", function(collection) {
 		return collection.getAllSorted().filter(function(item) {
 			return item.data.contentType == "campaign";
 		});
 	});
-  
+
 	// Return all events
 	eleventyConfig.addCollection("events", function(collection) {
 		return collection.getAllSorted().filter(function(item) {
 			return item.data.contentType == "event";
 		});
 	});
-  
+
 	// Return all games
 	eleventyConfig.addCollection("games", function(collection) {
 		return collection.getAllSorted().filter(function(item) {
 			return item.data.contentType == "game";
 		});
 	});
-  
+
 	// Return all battle reports
 	eleventyConfig.addCollection("battlereports", function(collection) {
 		return collection.getAllSorted().filter(function(item) {
 			return item.data.contentType == "battle-report";
 		});
 	});
+
+	return {
+		dir: {
+			input: "src",
+		}
+	};
 };
